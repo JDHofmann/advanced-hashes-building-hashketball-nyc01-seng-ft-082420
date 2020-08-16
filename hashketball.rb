23d42ebf87faad7d6b9415371e4bbfe653f4c41a
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-16 19:53:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,14 +2,38 @@ def game_hash_creator
 
 game_hash = {
   :home => {
-    :team_name => {},
-    :colors => {},
-    :players => {}
+    :team_name => "",
+    :colors => [],
+    :players => [
+      {
+        :player_name =>
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      },
+    ]
   },
   :away => {
-    :team_name => {},
-    :colors => {},
-    :players => {}
+    team_name => "",
+    :colors => [],
+    :players => [
+      {
+        :player_name =>
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists => 
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      },
+    ]
   },
 }
 
